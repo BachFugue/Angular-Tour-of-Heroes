563dd40a58ef7d3fbcdb19e83aba7549f5727034
Peter <peterlanier@me.com>
2019-04-26 19:53:38 -0600

    Completed step 2: Displaying a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Ymir' },
+	{ id: 12, name: 'Odin' },
+	{ id: 13, name: 'Frigg' },
+	{ id: 14, name: 'Thor' },
+	{ id: 15, name: 'Balder' },
+	{ id: 16, name: 'Tyr' },
+	{ id: 17, name: 'Bragi' },
+	{ id: 18, name: 'Loki' },
+	{ id: 19, name: 'Hel' },
+	{ id: 20, name: 'Heimdall' }
+];
